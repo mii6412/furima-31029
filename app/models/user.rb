72f99b846344889d703bd0321b9fd23e07536fa6
mii6412 <ai.takano12@gmail.com>
2020-11-06 00:32:41 +0900

    ログイン・オフ時の表示変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, password_length: 6..128
+         :recoverable, :rememberable, :validatable
+
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
 
   has_many :items
