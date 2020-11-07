268bb7fa36c7e5d9e304eae4ffc5124ac3d55258
mii6412 <ai.takano12@gmail.com>
2020-11-07 11:00:55 +0900

    Userモデルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+  NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+
   with_options presence: true do
     validates :nickname
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze}
-    NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
     validates :last_name, format: { with: NAME_REGEX }
     validates :first_name, format: { with: NAME_REGEX }
-    NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
     validates :last_name_kana, format: { with: NAME_KANA_REGEX}
     validates :first_name_kana, format: { with: NAME_KANA_REGEX}
     validates :birthday
