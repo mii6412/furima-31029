cabf9738d9cd1168f52dc94e0cdeb65dfc887def
mii6412 <ai.takano12@gmail.com>
2020-11-07 10:49:18 +0900

    ログイン機能の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, only: [:/]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-  def configure_permitted_parameters
-    if resource_class == User
+   def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
-     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
-    end
-  end
+   end
+
 end
