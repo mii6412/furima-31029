74e1f0c24941571b577821f66c40ec93ee5e2d53
mii6412 <ai.takano12@gmail.com>
2020-11-08 15:54:57 +0900

    itemモデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
   
   with_options presence: true do
+    validates :image
     validates :item_name
     validates :description 
     validates :category_id, numericality: { other_than: 1 } 
-    validates :condition_id 
-    validates :shipping_id
-    validates :departure_area_id
-    validates :duration_id
-    validates :departure_area_id
+    validates :condition_id, numericality: { other_than: 1 } 
+    validates :shipping_id, numericality: { other_than: 1 } 
+    validates :departure_area_id, numericality: { other_than: 1 } 
+    validates :duration_id, numericality: { other_than: 1 } 
+    validates :departure_area_id, numericality: { other_than: 1 } 
     validates :price
     validates :user
   end
